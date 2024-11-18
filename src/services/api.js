4b7da9e286cbf8f01bc04032886ba3ea4941e2ee
Mehtab Kausar <168742140+MehtabUppin@users.noreply.github.com>
2024-11-18 12:38:17 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://scanqr-jdez.onrender.com/api', // Your backend API base URL
+    baseURL: 'https://scanqr-jdez.onrender.com/api', // Your backend API base URL
 });
 
 export default api;
