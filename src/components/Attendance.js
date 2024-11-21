6fab5e5ad3f6d002fbb63000ea014e7c95434104
Satyasree341 <satyasreeg2002@gmail.com>
2024-11-21 16:06:39 +0530

    Update Attendance.js

--- src/components/Attendance.js
+++ src/components/Attendance.js
@@ -72,10 +72,12 @@ const Attendance = () => {
             console.log(currentHour)
             if (currentHour < 10 || currentHour > 11) {
                 setMessage("Morning login is only available between 9 AM and 9:30 AM.");
+                navigate('/attendance-type'); // Redirect on failure
                 return;
             }
             if (cookies.hasMarkedLogin) {
                 setMessage("You have already marked your morning login for today.");
+                navigate('/attendance-type'); // Redirect on failure
                 return;
             }
             setCookie('hasMarkedLogin', true, { path: '/', expires });
@@ -85,10 +87,12 @@ const Attendance = () => {
         if (attendanceType === 'lunch') {
             if (currentHour <= 12.5 || currentHour >= 14.5) {
                 setMessage("Lunch attendance can only be marked between 12:30 PM and 1:30 PM.");
+                navigate('/attendance-type'); // Redirect on failure
                 return;
             }
             if (cookies.hasMarkedLunch) {
                 setMessage("Lunch attendance can only be marked once per day.");
+                navigate('/attendance-type'); // Redirect on failure
                 return;
             }
             setCookie('hasMarkedLunch', true, { path: '/', expires });
@@ -98,6 +102,7 @@ const Attendance = () => {
         if (attendanceType === 'tea') {
             if (currentHour >=16 || currentHour <= 16.5) {
                 setMessage("Tea attendance can only be marked between 4:00 PM and 4:30 PM.");
+                navigate('/attendance-type'); // Redirect on failure
                 return;
             }
             if (cookies.hasMarkedTea) {
