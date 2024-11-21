f6f506b3f54e8c39d699585f108de3bea01ea8d5
Mehtab Kausar <168742140+MehtabUppin@users.noreply.github.com>
2024-11-21 08:27:18 +0530

    Update QRCodeScanner.js

--- src/components/QRCodeScanner.js
+++ src/components/QRCodeScanner.js
@@ -35,7 +35,7 @@ const QRCodeScanner = () => {
                 const instituteLongitude = 77.6014;
 
                 const distance = calculateDistance(latitude, longitude, instituteLatitude, instituteLongitude);
-                if (distance >= 50) {
+                if (distance <= 50) {
                     navigate('/attendance');
                 } else {
                     alert('You must be within a 50-meter radius of the institute.');
