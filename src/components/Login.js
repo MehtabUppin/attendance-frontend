a7520b6b1ef0b86cc1fb91cc4442814a09945977
Mehtab Kausar <168742140+MehtabUppin@users.noreply.github.com>
2024-11-22 12:47:42 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('https://scanqr-jdez.onrender.com/api/login', { email, password });
+            const response = await axios.post('https://modified-attendance-app.onrender.com/api/login', { email, password });
             const loginResponse = response.data;
 
             // Save token and user ID to local storage
